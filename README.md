72e3a3dd1294064d831cc42c6307597abf49a96d
fibo <casati_gianluca@yahoo.it>
2014-01-20 10:31:36 +0100

    print stack trace

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ strict-mode
 
 # Usage
 
-Suppose your *package.json main* attribute is the `npm init` default *index.js*,
+Suppose your *package.json main* attribute is the `npm init` default *index.js*.
+
+If you want that all the modules in your package has strict mode enabled,
 just wrap your *index.js* this way
 
 ```
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 // All credits goes to isaacs and its use-strict
 
-// module api is Locked, so will not change
+// module api is Locked, so it will not change
 // see http://nodejs.org/api/modules.html
 // that is why I just copyed and pasted the orig module wrapper
 var origWrapper         = '(function (exports, require, module, __filename, __dirname) { '
@@ -16,7 +16,7 @@ module.exports = function strictMode(callback) {
 
   // avoid nested require('strict-mode')
   if (strictModeExecuting)
-    throw new Error()
+    throw new Error('Nested strict-mode calls')
 
   strictModeExecuting = true
 
@@ -24,7 +24,12 @@ module.exports = function strictMode(callback) {
   module.wrapper[0] = strictWrapper
 
   // Every require in this callback will load modules in strict mode.
-  callback()
+  try {
+    callback()
+  }
+  catch(err) {
+    console.error(err.stack)
+  }
 
   // Restore orig module wrapper, play well with others.
   module.wrapper[0] = origWrapper
